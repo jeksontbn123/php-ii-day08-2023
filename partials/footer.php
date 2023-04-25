91ff84100201cb0833decb98e0e4d575bd022473
Jekson <100131109+jeksontbn123@users.noreply.github.com>
2023-04-25 16:20:54 +0700

    update day08

---
+++ partials/footer.php
@@ -0,0 +1,3 @@
+            <footer class="p-4 bg-gray-300">
+            <p>Copyright &copy; 2023 - Developted by <b>Jek.Tabuni</b></p>
+           </footer>
---
+++ partials/header.php
@@ -0,0 +1,4 @@
+            <header class="p-4 bg-gray-300">
+                <h1 class="text-5xl font-bold">DataBase Siswa</h1>
+                <p>Sekolah Coding Papua</p>
+           </header>
